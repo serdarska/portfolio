9d33bd053a2321843c06712154f682186cadebba
serdarska <serdarska.jovana.js@gmail.com>
2022-07-04 12:38:19 +0200

    added files in assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
@@ -16,7 +16,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 
@@ -24,9 +23,11 @@ nav {
     font-weight: bold;
     color: #2c3e50;
 
-    &.router-link-exact-active {
+    &
+    .router-link-exact-active {
       color: #42b983;
     }
+
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import bootstrap from './assets/scss/bootstrap.scss'
+import Navbar from "./components/layout/Navbar.vue";
 
 Vue.config.productionTip = false
 
