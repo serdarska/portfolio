6174741c50f66fb6975c51105597a5b98345de56
serdarska <serdarska.jovana.js@gmail.com>
2022-07-07 14:02:02 +0200

    added design

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,29 +2,29 @@
   <section>
     <div class="row">
       <div class="col-12 text-center">
-        <button @click="buttonClick" type="button" class="btn btn-secondary btn-sm">Click me :)</button>
-        <p>{{ addressChange }}</p>
+        <button @click="buttonClick" type="button" class="btn btn-danger btn-sm">Click me!</button>
+        <p class="fs-3">{{ addressChange }}</p>
       </div>
       <div class="col-12 col-md-4  my-3 text-center">
         <div class="d-flex justify-content-center align-items-center">
           <i class="fa fa-map-marker fa-2x text-col" aria-hidden="true"></i>
           <p class="m-0 fs-3 text-color">Where to find me</p>
         </div>
-        <p class="case-change">{{ address }}</p>
+        <p class="case-change fs-4">{{ address }}</p>
       </div>
       <div class="col-12 col-md-4 my-3 text-center">
         <div class="d-flex justify-content-center align-items-center">
           <i class="fa fa-envelope fa-2x text-col" aria-hidden="true"></i>
           <p class="m-0 fs-3 text-color">Email me at</p>
         </div>
-        <p class="case-change">{{ email }}</p>
+        <p class="case-change fs-4">{{ email }}</p>
       </div>
       <div class="col-12 col-md-4 my-3 text-center">
         <div class="d-flex justify-content-center align-items-center">
           <i class="fa fa-phone fa-2x text-col" aria-hidden="true"></i>
           <p class="m-0 fs-3 text-color">Call me at</p>
         </div>
-        <p>{{ phone }}</p>
+        <p class="fs-4 case-change">{{ phone }}</p>
       </div>
     </div>
   </section>
@@ -36,6 +36,7 @@ export default {
   methods: {
     buttonClick() {
       this.upperCase = true
+      this.bold = true
     }
   },
   computed: {
@@ -49,6 +50,7 @@ export default {
       const elements = document.getElementsByClassName("case-change");
       for (let element of elements) {
         element.style.textTransform = 'uppercase'
+        element.style.fontWeight = 'bold'
       }
     }
   },
@@ -58,6 +60,7 @@ export default {
       email: "serdarska.jovana.js@gmail.com",
       phone: "+389-70/354/184",
       upperCase: false,
+      bold : false,
       message: "That's better. :)"
     }
   }
