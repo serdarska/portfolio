db2d3c0011acc8e796d4f548755e8e037afab038
serdarska <serdarska.jovana.js@gmail.com>
2022-07-07 14:59:09 +0200

    container

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <main class="container bg-light">
+  <main class="container bg-light border border-5 border-danger">
     <name-surname/>
     <about/>
     <profile/>
