1a3659cda63b7da63b072ce1d8c80c1c90ffabb1
serdarska <serdarska.jovana.js@gmail.com>
2022-07-08 14:02:25 +0200

    added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="row bg-black">
+      <nav class="navbar navbar-dark">
+        <p class="p-0 m-0 fs-2 text-color">My CV</p>
+      </nav>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
